f0eee0766130df701a9ef3933cca7ea7ee17b575
Mining Geologist <127529341+Mining-Geologist@users.noreply.github.com>
2026-07-15 22:44:09 -0400

    Expose clustered structural domain builder

--- python/src/polatory/__init__.py
+++ python/src/polatory/__init__.py
@@ -1,3 +1,7 @@
 from ._core import *
 from ._core import __doc__, __version__
 from ._structural import *
+from .clustered_domain_builder import (
+    ClusteredStructuralDomainBuilder3,
+    fit_from_meshes_clustered,
+)
