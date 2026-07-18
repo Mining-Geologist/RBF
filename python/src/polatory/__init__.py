73a27bd6e64a8a9aff4f2f0e66933cc08896387c
Mining Geologist <127529341+Mining-Geologist@users.noreply.github.com>
2026-07-18 01:47:44 -0400

    Export automatic SubDomainer API

--- python/src/polatory/__init__.py
+++ python/src/polatory/__init__.py
@@ -1,6 +1,11 @@
 from ._core import *
 from ._core import __doc__, __version__
 from ._structural import *
+from .automatic_domain_builder import (
+    AutomaticStructuralDomainBuilder3,
+    AutomaticStructuralDomainDiagnostics3,
+    LeapfrogAutomaticDomainBuilder3,
+)
 from .clustered_domain_builder import (
     ClusteredStructuralDomainBuilder3,
     fit_from_meshes_clustered,
