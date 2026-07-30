8012e6ab83b6b9c7dbf099a3faa1021a9be18aeb
Mining Geologist <127529341+Mining-Geologist@users.noreply.github.com>
2026-07-30 13:00:51 -0400

    Export heap-corrected automatic domain builder

--- python/src/polatory/__init__.py
+++ python/src/polatory/__init__.py
@@ -2,7 +2,7 @@ from ._core import *
 from ._core import __doc__, __version__
 from ._structural import *
 from .automatic_domain_builder import AutomaticStructuralDomainDiagnostics3
-from .leapfrog_automatic_domain_builder import (
+from .leapfrog_automatic_domain_builder_fixed import (
     AutomaticStructuralDomainBuilder3,
     LeapfrogAutomaticDomainBuilder3,
 )
